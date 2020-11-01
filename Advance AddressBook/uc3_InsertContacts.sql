9075541b6f1026d47444b064d86a09cf2d3af25f
Soumya <soumyadipta0077@gmail.com>
2020-11-01 21:33:31 +0530

    [Soumya] Add: Inserting values into table

---
+++ Advance AddressBook/uc3_InsertContacts.sql
@@ -0,0 +1,3 @@
+insert into AddressBook values
+('Soumya','Banerjee','Berhampore123','Berhampore','West Bengal',123,99999,'abc@gmail.com'),
+('Ranbir','Kapoor','Mumbai123','Mumbai','Maharashtra',123,88888,'abcd@gmail.com')
