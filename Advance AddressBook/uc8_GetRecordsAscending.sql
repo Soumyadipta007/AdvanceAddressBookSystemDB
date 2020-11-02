65dd85a8a4572e64cc6cd206921cded3e3f99997
Soumya <soumyadipta0077@gmail.com>
2020-11-02 13:55:41 +0530

    [Soumya] Add: Get Records Ascending

---
+++ Advance AddressBook/uc8_GetRecordsAscending.sql
@@ -0,0 +1,4 @@
+insert into AddressBook values
+('Shahrukh','Khan','Mumbai123','Mumbai','Maharashtra',381,777777,'srk123@gmail.com')
+
+select * from AddressBook where city ='Mumbai' order by firstName
