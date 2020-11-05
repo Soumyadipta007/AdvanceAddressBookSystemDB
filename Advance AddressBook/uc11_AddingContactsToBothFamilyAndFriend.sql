a96a79dd829fb91acc389763f26915f9681e1e15
Soumya <soumyadipta0077@gmail.com>
2020-11-05 14:23:47 +0530

    [Soumya] Refactor: Updating Contacts To Both Family And Friend

--- Advance AddressBook/uc11_AddingContactsToBothFamilyAndFriend.sql
+++ Advance AddressBook/uc11_AddingContactsToBothFamilyAndFriend.sql
@@ -1,5 +1,9 @@
 insert into AddressBook values
-('Pranoy','Pal','Kolkata123','Kolkata','West Bengal',729,76788,'pranoy@gmail.com','PranoyBhp','Family'),
-('Pranoy','Pal','Kolkata123','Kolkata','West Bengal',729,76788,'pranoy@gmail.com','PranoyBhp','Friend'),
-('Abhro','G','Kolkata123','Kolkata','West Bengal',727,76688,'abh@gmail.com','AbhBhp','Family'),
-('Abhro','G','Kolkata123','Kolkata','West Bengal',727,76688,'abh@gmail.com','AnhBhp','Friend')
+('Pranoy','Pal','Kolkata123','Kolkata','West Bengal',729,76788,'pranoy@gmail.com','Family','PranoyBhp'),
+('Pranoy','Pal','Kolkata123','Kolkata','West Bengal',729,76788,'pranoy@gmail.com','Friend','PranoyBhp'),
+('Abhro','G','Kolkata123','Kolkata','West Bengal',727,76688,'abh@gmail.com','Family','AbhBhp'),
+('Abhro','G','Kolkata123','Kolkata','West Bengal',727,76688,'abh@gmail.com','Friend','AbhBhp'),
+('Tandra','Banerjee','Berhampore123','Berhampore','West Bengal',123,77777,'tandra@gmail.com','Family','Mom'),
+('Pradipta','Banerjee','Berhampore123','Berhampore','West Bengal',123,78888,'pradip@gmail.com','Family','Father'),
+('Rounak','Ghosh','Kolkata123','Kolkata','West Bengal',345,77888,'rou@gmail.com','Friend','RounakJU'),
+('Ramij','Ali','Delhi123','Delhi','Haryana',789,77788,'ramij@gmail.com','Friend','RamijJU')
