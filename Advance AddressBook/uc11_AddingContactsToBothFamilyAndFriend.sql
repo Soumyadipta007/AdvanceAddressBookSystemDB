9669c9bd928ee13f2c4450f92dd7f8d5159a7423
Soumya <soumyadipta0077@gmail.com>
2020-11-05 13:32:54 +0530

    [Soumya] Add: Adding Contacts To Both Family And Friend

---
+++ Advance AddressBook/uc11_AddingContactsToBothFamilyAndFriend.sql
@@ -0,0 +1,5 @@
+insert into AddressBook values
+('Pranoy','Pal','Kolkata123','Kolkata','West Bengal',729,76788,'pranoy@gmail.com','PranoyBhp','Family'),
+('Pranoy','Pal','Kolkata123','Kolkata','West Bengal',729,76788,'pranoy@gmail.com','PranoyBhp','Friend'),
+('Abhro','G','Kolkata123','Kolkata','West Bengal',727,76688,'abh@gmail.com','AbhBhp','Family'),
+('Abhro','G','Kolkata123','Kolkata','West Bengal',727,76688,'abh@gmail.com','AnhBhp','Friend')
